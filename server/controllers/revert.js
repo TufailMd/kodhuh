55560e4cd4c61318588b28149398e2bc627fc74d
TufailMd <jrtufailmd@gmail.com>
2026-06-23 05:14:46 +0530

    feat: implement revertRepo

--- server/controllers/revert.js
+++ server/controllers/revert.js
@@ -1,3 +1,25 @@
-export function revertRepo() {
-  console.log("revert cmd called");
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+const readdir = promisify(fs.readdir);
+const copyFile = promisify(fs.copyFile);
+import { s3, S3_BUCKET } from "../config/aws-config.js";
+
+export async function revertRepo(commitID) {
+  const repoPath = path.resolve(process.cwd(), ".kod");
+  const commitsPath = path.join(repoPath, "commits");
+  const commitDir = path.join(commitsPath, commitID);
+
+  try {
+    const files = await readdir(commitDir);
+    const parentDir = path.resolve(repoPath, "..");
+
+    for (const file of files) {
+      await copyFile(path.join(commitDir, file), path.join(parentDir, file));
+    }
+
+    console.log(`Commit ${commitID} reverted successfully.`);
+  } catch (error) {
+    console.error("Unable to revert", error);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -46,7 +46,9 @@ yargs(hideBin(process.argv))
         type: "string",
       });
     },
-    revertRepo,
+    (argv) => {
+      revertRepo(argv.commitID);
+    },
   )
   .demandCommand(1, "You need at least one command")
   .help().argv;
