7454a43db1f6ea2eece2e494b9ae0d09862d237c
TufailMd <jrtufailmd@gmail.com>
2026-06-23 04:50:11 +0530

    feat: implement pullRepo

--- server/controllers/pull.js
+++ server/controllers/pull.js
@@ -1,3 +1,38 @@
-export function pullRepo() {
-  console.log("pull cmd called");
+import fs from "fs/promises";
+import path from "path";
+import { s3, S3_BUCKET } from "../config/aws-config.js";
+
+export async function pullRepo() {
+  const repoPath = path.resolve(process.cwd(), ".kod");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const data = await s3
+      .listObjectsV2({
+        Bucket: S3_BUCKET,
+        Prefix: "commits/",
+      })
+      .promise();
+
+    const objects = data.Contents;
+
+    for (const object of objects) {
+      const key = object.Key;
+
+      const commitDir = path.join(commitsPath, key.split("/")[1]);
+
+      await fs.mkdir(commitDir, { recursive: true });
+
+      const params = {
+        Bucket: S3_BUCKET,
+        Key: key,
+      };
+
+      const fileContent = await s3.getObject(params).promise();
+      await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+    }
+    console.log("All commits pulled from S3.");
+  } catch (error) {
+    console.log(`Unable to pull from S3 : ${error}`);
+  }
 }
