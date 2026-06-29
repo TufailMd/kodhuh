7d9201b8d08f74add0bbbedbd996794116a20eb1
TufailMd <jrtufailmd@gmail.com>
2026-06-29 18:16:23 +0530

    feat(user): implement authentication and complete user CRUD operations

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,8 +15,19 @@ const connectClient = async () => {
   }
 };
 
-const getAllUsers = (req, res) => {
-  res.send("All users fetched");
+const getAllUsers = async (req, res) => {
+  try {
+    await connectClient();
+    const db = client.db("kodbase");
+    const usersCollection = db.collection("users");
+
+    const users = await usersCollection.find().toArray();
+
+    res.json(users);
+  } catch (err) {
+    console.error("Error during fetching users:", err, err.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 const signUp = async (req, res) => {
@@ -38,7 +49,10 @@ const signUp = async (req, res) => {
     const user = { username, email, password: hashedPassword };
     const result = await usersCollection.insertOne(user);
 
-    const token = jwt.sign({ id: result.insertedId }, process.env.JWT_SECRET_KEY);
+    const token = jwt.sign(
+      { id: result.insertedId },
+      process.env.JWT_SECRET_KEY,
+    );
 
     res.json({ token });
   } catch (err) {
@@ -48,20 +62,114 @@ const signUp = async (req, res) => {
   }
 };
 
-const logIn = (req, res) => {
-  res.send("Logging in");
+const logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    await connectClient();
+    const db = client.db("kodbase");
+    const usersCollection = db.collection("users");
+    const user = await usersCollection.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Your password is incorrect" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, userId: user._id });
+  } catch (err) {
+    console.error("Error during log in:", err, err.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
-const getUserProfile = (req, res) => {
-  res.send("Profile fetched");
+const getUserProfile = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await connectClient();
+    const db = client.db("kodbase");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.error("Error during fetching user profile:", err);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
-const updateUserProfile = (req, res) => {
-  res.send("Profile updated");
+const updateUserProfile = async function (req, res) {
+  const { id } = req.params;
+  const { email, password } = req.body;
+
+  let updateFields = { email };
+
+  if (password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    updateFields.password = hashedPassword;
+  }
+
+  try {
+    await connectClient();
+    const db = client.db("github-clone");
+    const usersCollection = db.collection("users");
+
+    const result = await usersCollection.findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      { $set: updateFields },
+      { returnDocument: "after" },
+    );
+
+    if (!result.value) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.send(result.value);
+  } catch (err) {
+    console.error("Error during updating:", err, err.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
-const deleteUserProfile = (req, res) => {
-  res.send("Profile deleted");
+const deleteUserProfile = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await connectClient();
+    const db = client.db("kodbase");
+    const usersCollection = db.collection("users");
+
+    const user = await usersCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User profile deleted" });
+  } catch (err) {
+    console.error("Error during fetching user profile:", err);
+    console.error("Error during deletion:", err, err.message);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export default {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,10 +4,10 @@ import userController from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.get("/allUsers", userController.getAllUsers);
-userRouter.get("/userProfile", userController.getUserProfile);
+userRouter.get("/userProfile/:id", userController.getUserProfile);
 userRouter.post("/logIn", userController.logIn);
 userRouter.post("/signUp", userController.signUp);
-userRouter.put("/updateUserProfile", userController.updateUserProfile);
-userRouter.delete("/deleteUserProfile", userController.deleteUserProfile);
+userRouter.put("/updateUserProfile/:id", userController.updateUserProfile);
+userRouter.delete("/deleteUserProfile/:id", userController.deleteUserProfile);
 
 export default userRouter;
